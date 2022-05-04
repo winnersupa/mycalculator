9ba113557cc71c56a09a1122c87cd56325e339dc
winnersupa <93438184+winnersupa@users.noreply.github.com>
2022-05-04 09:54:09 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+  include_package_data = true,
+  name='mycalc0001',
+  version='0.0.1',
+  description='oss-dev my calculator 0001',
+  author='winnersupa'
+  author_email='winnersupa@naver.com'
+  url="https://github.com/winnersupa/mycalculator",
+  douwnload
